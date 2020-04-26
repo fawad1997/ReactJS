d5c21aa20bc277e0a6f3198cc10228db7103d6d6
unknown <fawad_12@outlook.com>
2020-04-26 14:53:48 +0500

    created store and root reducer

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import RootReducer from './reducers/RootReducer';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
+const store = createStore(RootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'));
---
+++ myapp/src/reducers/RootReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    posts: []
+}
+const RootReducer = (state = initialState,action) => {
+    return state;
+}
+
+export default RootReducer;
